c4f329666ec49fd0eac612a28ecf042f21f77035
Shubham <shubham07shastri70@gmail.com>
2020-08-11 13:05:12 +0530

    fifth commit

--- baked_byheart/settings.py
+++ baked_byheart/settings.py
@@ -24,7 +24,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'nctdiz(bcxi+a4&ccz0kz86k7tic+x^(62n4sumex67-*0xjul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
